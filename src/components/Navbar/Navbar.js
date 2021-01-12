36de25f6257f2be33f61fdc4a31ef328952fb4a0
mathiuskormasela12 <mathiuskormasela12@gmail.com>
2021-01-12 13:44:19 +0700

    Recreate Navbar Component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,26 +10,30 @@ import styled from './style.module.scss';
 import { Container } from '../container/Container';
 
 // Import all images
-import tickitz from '../../assets/img/tickitz2.svg';
+import tickitz from '../../assets/img/tickitz.png';
 import burger from '../../assets/img/burger.svg';
 
 export default function Navbar(props) {
-  const [isShow, ShowInput] = useState(false);
+  const [state, setState] = useState({
+    showDropdown: false
+  });
+
+  const show = prop => {
+    setState(currentState => ({
+      ...currentState,
+      [prop]: !currentState[prop]
+    }));
+  }
+
   return (
     <React.Fragment>
       <nav className={ styled.nav }>
-        <Container component="navbar">
-          <div className={ styled.container }>
+        <Container>
+          <div className={ styled.navContainer }>
             <Link to="/" className={ styled.brand }>
-              <img src={ tickitz } alt="Tickitz Logo" />
+              <img src={ tickitz } className={ styled.img } alt="Tickitz Logo" />
             </Link>
-            <ul className={ styled.navList } style={ (isShow) ? ({ width: '45%'}) : null }>
-              <li className={ styled.navItem }>
-                <div className={ styled.formContainer }>
-                  <input type="search" className={ styled.input } placeholder="Search here..." />
-                  <FaSearch className={ styled.searchMobile } />
-                </div>
-              </li>
+            <ul className={ styled.navList }>
               <li className={ styled.navItem }>
                 <Link to="/">
                   Movies
@@ -46,41 +50,34 @@ export default function Navbar(props) {
                 </Link>
               </li>
             </ul>
-            <div className={ styled.navSide } style={ (isShow) ? ({ width: '40%'}) : null }>
+            <div className={ styled.navSide }>
               <div className={ styled.dropdown }>
-                <div className={ styled.link }>
+                <Link to="/" onClick={ () => show('showDropdown') }>
                   Location
-                  <FaAngleDown className={ styled.arrow } />
-                  <ul className={ styled.dropdownList}>
-                    <li className={ styled.dropdownItem }>
-                      <Link to="/">
-                        Jakarta
-                      </Link>
-                    </li>
-                    <li className={ styled.dropdownItem }>
-                      <Link to="/">
-                        Tokyo
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
+                  <FaAngleDown className={ styled.arrow }/>
+                </Link>
+                <ul className={ `${styled.dropdownList} ${state.showDropdown ? styled.showDropdown : ''}` }>
+                  <li className={ styled.item }>
+                    <Link to="/">
+                      Jakarta
+                    </Link>
+                  </li>
+                  <li className={ styled.item }>
+                    <Link to="/">
+                      Surabaya
+                    </Link>
+                  </li>
+                </ul>
               </div>
-              <div className={ styled.searchBar } style={ (isShow) ? ({ marginLeft: '2rem'}) : null }>
-                <form>
-                  {
-                    isShow ? (
-                      <input type="search" className={ styled.input } placeholder="Search here..." />
-                    ) : null
-                  }
-                  <FaSearch className={ styled.search } onClick={() => ShowInput(c => !c)}/>
+              <div className={ styled.searchBar }>
+                <form className={ styled.form }>
+                  <input type="search" className={ styled.inputSearch } placeholder="Search movie here ..."/>
                 </form>
+                <FaSearch className={ styled.search } />
               </div>
               <div className={ styled.btnBar }>
-                <Link to="/register" className={ styled.btn } >Sign Up</Link>
+                <Link to="/register" className={ styled.btn }>Sign Up</Link>
               </div>
-            </div>
-            <div className={ styled.burger }>
-              <img src={ burger } alt="Burger" />
             </div>
           </div>
         </Container>
